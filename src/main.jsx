e17f29ade1bb3bd1b520443d7ad099eb02c3bc8d
VictoriaGleam <Radbergvictoria@gmail.com>
2025-11-06 10:52:37 +0100

    Initial commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import "./index.css";   // <- den här MÅSTE finnas i src/
-
+import { HashRouter } from "react-router-dom";
+import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <HashRouter basename="/">
+      <App />
+    </HashRouter>
   </React.StrictMode>
 );
